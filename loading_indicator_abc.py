e8fa6d7262990d7dc5d49241b96916d6a884756a
Grayson <grayson.miller124@gmail.com>
2023-05-02 02:26:16 +0000

    abc version

--- loading_indicator_abc.py
+++ loading_indicator_abc.py
@@ -1,12 +1,36 @@
 import asyncio
 import signal
+from abc import ABC, abstractmethod
 
 
-class LoadingIndicator:
-    """Displays a loading indicator in the CLI using Unicode Braille characters."""
-    def __init__(self, interval=0.05, br_dots=None, done_callback=None, step_callback=None):
+class LoadingIndicator(ABC):
+    """Abstract base class for loading indicators."""
+    def __init__(self, interval=0.05):
         """
         Creates a loading indicator object.
+
+        Args:
+            interval (float): Time interval in seconds between each step of the loading indicator.
+        """
+        self.interval = interval
+        self.counter = 0
+        self.is_done = False
+
+    @abstractmethod
+    async def run(self):
+        """Runs the loading indicator."""
+        pass
+
+    def done(self):
+        """Signals that the loading indicator is done."""
+        self.is_done = True
+
+
+class BrailleLoadingIndicator(LoadingIndicator):
+    """Displays a loading indicator in the CLI using Unicode Braille characters."""
+    def __init__(self, br_dots=None, done_callback=None, step_callback=None, **kwargs):
+        """
+        Creates a BrailleLoadingIndicator object.
 
         Args:
             br_dots (list of str): List of Unicode Braille characters to use for the loading indicator.
@@ -15,11 +39,10 @@ class LoadingIndicator:
                 Defaults to printing the number of steps taken.
             step_callback (callable): Callback function to call on each step of the loading indicator.
                 Defaults to printing the Braille character for the current step.
+            **kwargs: Additional arguments to pass to the parent class constructor.
         """
+        super().__init__(**kwargs)
         self.br_dots = br_dots or ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
-        self.interval = interval
-        self.counter = 0
-        self.is_done = False
         self.done_callback = done_callback or self.default_done_callback
         self.step_callback = step_callback or self.default_step_callback
 
@@ -40,10 +63,6 @@ class LoadingIndicator:
     def default_done_callback(num_steps):
         """Default done callback function that prints the number of steps taken."""
         print(f"\nDone after {num_steps} steps.")
-
-    def done(self):
-        """Signals that the loading indicator is done."""
-        self.is_done = True
 
 
 def progress(indicator):
@@ -67,7 +86,6 @@ def progress(indicator):
     return wrapped_callback
 
 
-
 async def main():
     try:
         # Customize the loading indicator with custom callbacks
@@ -77,18 +95,24 @@ async def main():
         def custom_step_callback(braille_char):
             print(f"\rStep: {braille_char}", end="")
 
-        indicator = LoadingIndicator(done_callback=custom_done_callback)# , step_callback=custom_step_callback)
+        indicator = BrailleLoadingIndicator(done_callback=custom_done_callback)#, step_callback=custom_step_callback)
 
         # Add a signal handler to the event loop to catch keyboard interrupts
         loop = asyncio.get_running_loop()
         loop.add_signal_handler(signal.SIGINT, indicator.done)
 
+        # Define a coroutine function that simulates some long-running task
         @progress(indicator)
         async def my_task():
-            await asyncio.sleep(2)
-            return "RESULT"
+            await asyncio.sleep(1)
+            return "Task result"
 
-        print(f"\n{await my_task()}")
+        # Wrap the coroutine function with the progress decorator
+
+        # Run the decorated task
+        result = await my_task()
+        print(f"\nTask result: {result}")
+        
     finally:
         # Remove the signal handler when the program is done
         loop.remove_signal_handler(signal.SIGINT)
