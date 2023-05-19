9564b4c928564a67a0dad776acc93d48a257e540
Grayson <grayson.miller124@gmail.com>
2023-05-19 08:12:33 +0000

    Poetry `gematrick` and data folder

---
+++ gematrick/gematrick/__init__.py
@@ -0,0 +1,3 @@
+from .__main__ import BookData
+
+__all__ = ["BookData"]
--- gematrick/gematrick/__main__.py
+++ gematrick/gematrick/__main__.py
@@ -1,88 +1,49 @@
 import functools
+from pathlib import Path
 from hebrew import Hebrew, GematriaTypes
 import grapheme
 
 import json
 
-special_words = {
-    "torah": (
-        Hebrew("תורה"),
-        611,
-    ),  # gematria 611 - 611 commandments, 2 for the 2 commandments that were said by God = 613
-    "yh": (Hebrew("יה"), 15),
-    "el": (Hebrew("אל"), 31),
-    "israel": (Hebrew("ישראל"), 541),
-    "moses": (Hebrew("משה"), 345),
-    # "the man": (Hebrew("האדם"), 45),
-}
-
-
-def get_special_gematrias():
-    return {k: v[0].gematria() for k, v in special_words.items()}
-
-
-def assert_special_gematrias():
-    for k, v in special_words.items():
-        assert (
-            v[0].gematria() == v[1]
-        ), f"{k} gematria is {v[0].gematria()}, should be {v[1]}"
-
-
-TORAH = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]
-NEVIIM = [
-    "Joshua",
-    "Judges",
-    "I Samuel",
-    "II Samuel",
-    "I Kings",
-    "II Kings",
-    "Isaiah",
-    "Jeremiah",
-    "Ezekiel",
-    "Hosea",
-    "Joel",
-    "Amos",
-    "Obadiah",
-    "Jonah",
-    "Micah",
-    "Nahum",
-    "Habakkuk",
-    "Zephaniah",
-    "Haggai",
-    "Zechariah",
-    "Malachi",
-]
-KETUVIM = [
-    "Psalms",
-    "Proverbs",
-    "Job",
-    "Song of Solomon",
-    "Ruth",
-    "Lamentations",
-    "Ecclesiastes",
-    "Esther",
-    "Daniel",
-    "Ezra",
-    "Nehemiah",
-    "I Chronicles",
-    "II Chronicles",
-]
-
-TANACH = TORAH + NEVIIM + KETUVIM
+from gematrick.books import TORAH, NEVIIM, KETUVIM, TANACH
 
 
 class BookData:
-    def __init__(self, books=TANACH):
+    def __init__(self, books="tanach"):
+        self.data = None
+        self.books = None
+
+        self.data_path = Path(__file__).parent / "data"
+
+        self.__load(group=books)
+
+    def __load(self, group="tanach"):
+        """Load books of Tanach from based on grouping.
+
+        Parameters
+        ----------
+        group : str
+            Grouping of books to load. Valid values are "Tanach", "Torah", "nevi'im", "Ketuvim" and "Book" by name.
+
+        """
+        with open(self.data_path / "books.json", "r") as f:
+            data = json.load(f)
+            tanach_data = data["torah"] + data["neviim"] + data["ketuvim"]
+            print(tanach_data)
+            if group == "Tanach":
+                self.books = tanach_data
+            elif group in ["Torah", "Nevi'im", "Ketuvim"]:
+                group = group.lower().replace("'", "")
+                self.books = data[group]
+            elif group in tanach_data:
+                self.books = [group]
+            else:
+                raise ValueError(f"Invalid Tanach grouping: {group}")
+
         self.data = []
-        self.books = books
-
-        self.__load_books()
-
-    def __load_books(self):
-        with open("hebrew.json", "r") as f:
+        with open(self.data_path / "tanach.json", "r") as f:
             data = json.load(f)
             for book in self.books:
-                print(f"Loading {book}...")
                 self.data.extend(data[book])
 
     def chapters(self):
@@ -94,6 +55,13 @@ class BookData:
             for verse in chapter:
                 yield verse
 
+    @property
+    def verse_slices(self):
+        """Return a generator of verse slices, where each slice is a list of words in the verse."""
+        for verse in self.verses():
+            v = [word[0] for word in verse]
+            yield v
+
     def words(self):
         for verse in self.verses():
             for word in verse:
@@ -104,22 +72,24 @@ class BookData:
             yield word[0]
 
     def as_hebrew(self):
+        """Not efficient, use BookData.to_hebrew_string() instead."""
         for word in self.just_words():
             yield Hebrew(word)
 
-    @functools.lru_cache(maxsize=128)
+    @functools.lru_cache()
     def to_hebrew_string(self):
         return Hebrew(self.__str__())
 
     def gematria(self):
         return self.to_hebrew_string().gematria()
 
+    @functools.lru_cache()
     def __str__(self) -> str:
         return " ".join(self.just_words())
 
     def __len__(self):
         """Return total books that were loaded."""
-        return len(self.data)
+        return len(self.books)
 
     def __contains__(self, item):
         """Return True if item is in the list of books."""
@@ -130,31 +100,16 @@ def main():
     # print("Special words gematrias:", get_special_gematrias())
     assert_special_gematrias()
 
-    torah_data = BookData(books=TORAH)
-    # neviim_data = BookData(books=NEVIIM)
-    # ketuvim_data = BookData(books=KETUVIM)
+    torah_data = BookData(books="torah")
+    # neviim_data = BookData(books="neviim")
+    # ketuvim_data = BookData(books="ketuvim")
 
-    # tanach_data = BookData()
+    tanach_data = BookData()
 
     # l = tanach_data.to_hebrew_string().text_only().length
 
-    if "Genesis" in torah_data:
-        hs = torah_data.as_hebrew()
-        word_sum = 0
-        roshei_tevot_sum = 0
-        for _ in range(7):
-            h = next(hs)
-            word_sum += h.gematria()
-            roshei_tevot_sum += Hebrew(h.slice(0, 1)).gematria()
-        assert (
-            word_sum == 2701
-        ), f"sum of first 7 words of Genesis is {word_sum}, should be 2701"
-        assert (
-            roshei_tevot_sum == 22
-        ), f"sum of roshei tevot of first 7 words of Genesis is {roshei_tevot_sum}, should be 22"
-        print("All test gematrias are correct!")
-
-    print("\n", torah_data.gematria())
+    print(torah_data.verse_slices[0:5])
+    # dump tanach data to json well formatted
 
 
 if __name__ == "__main__":
