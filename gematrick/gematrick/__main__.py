9daeef9bab10eff5105043e212c131e214cb2448
Grayson <grayson.miller124@gmail.com>
2023-05-21 07:10:53 +0000

    O.o

--- gematrick/gematrick/__main__.py
+++ gematrick/gematrick/__main__.py
@@ -29,8 +29,45 @@ def babel_investigation(torah_data: BookData):
     assert Hebrew(gen114).gematria() == 3535
 
     print("Genesis 11:4", gen114)
-    print("Letter #12856", torah_data.letters[12856])
+    babel_aleph_start = 12856
     # generate aleph tower for 1850 (50*37) ELS
+    while torah_data.letters[babel_aleph_start] == "א":
+        babel_aleph_start += 1847
+
+        # print 5 previous + 1 current + 5 next
+        prev = "".join(torah_data.letters[babel_aleph_start - 5 : babel_aleph_start])
+        current = torah_data.letters[babel_aleph_start]
+        next = "".join(
+            torah_data.letters[babel_aleph_start + 1 : babel_aleph_start + 6]
+        )
+        print(
+            f"Letter #{babel_aleph_start}",
+            f"{prev} {current} {next}",
+        )
+
+
+def aleph_torah_experiment(torah_data: BookData):
+    # get all alephs in torah
+    indices = []
+    for i, letter in enumerate(torah_data.letters):
+        if letter == "א":
+            indices.append(i)
+    deltas = [indices[i + 1] - indices[i] for i in range(len(indices) - 1)]
+    dmax = max(deltas)
+    # normalize deltas
+    deltas = [d / dmax for d in deltas]
+    print(deltas)
+
+    # ensure perfect decimal repetition, i.e. 0.123123123123... not 0.123123123124...
+    def is_repeating_decimal(f: float) -> bool:
+        # find longest substring that repeats
+        s = str(f)[2:]
+        for i in range(1, len(s)):
+            if s[:i] * (len(s) // i) == s:
+                return True
+        return False
+
+    print(all([is_repeating_decimal(d) for d in deltas]))
 
 
 def main():
@@ -46,6 +83,7 @@ def main():
     # torah = torah_data.to_hebrew_string().text_only()
 
     babel_investigation(torah_data)
+    aleph_torah_experiment(torah_data)
 
 
 if __name__ == "__main__":
