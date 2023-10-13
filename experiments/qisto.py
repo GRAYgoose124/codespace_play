e0cca4dcce5bdefae24bbe00fed4fe40cbe9fe75
Grayson Miller <grayson.miller124@gmail.com>
2023-10-12 22:19:23 -0600

    work towards the multiplexed qc learner

--- experiments/qisto.py
+++ experiments/qisto.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 import torch, torch.nn as nn, torch.optim as optim
 from qiskit import Aer, QuantumCircuit, transpile, assemble, execute
@@ -63,19 +64,95 @@ class QcNet(nn.Module):
         return torch.softmax(self.fc3(x), dim=1)
 
 
+class QcNetTrainer:
+    def __init__(self, C):
+        self.C = C
+        self.model = QcNet(C)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.01)
+        self.criterion = nn.MSELoss()
+
+        self._qcdsls = []
+
+    @property
+    def circuits(self):
+        return self._qcdsls
+
+    def add_dsl(self, dsl):
+        self._qcdsls.append(QcDSL(dsl, self.C))
+
+    def train(self, inputs, labels):
+        for epoch in range(5000):
+            self.optimizer.zero_grad()
+            outputs = self.model(inputs)
+            loss = self.criterion(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+
+    def infer(self, circuit, **kwargs):
+        # figure out arg order by inspecting the dsl for kwargs
+        args = []
+        for line in self.circuits[circuit].dsl.splitlines():
+            line = line.strip()
+            if line:
+                tokens = line.split(",")
+                for token in tokens[1:]:
+                    token = token.strip()
+                    if token in kwargs:
+                        args.append(kwargs[token])
+
+        # infer
+        with torch.no_grad():
+            prediction = self.model(torch.tensor([args]))
+            print(
+                f"For {args=}, predicted probabilities: {prediction.numpy()[0]}\n"
+                f"Actual probabilities: {self.circuits[circuit].get_quantum_output(kwargs)}\n"
+            )
+
+    def gen_data_from_linspace(self, circuit, linspaces, mapping):
+        """
+        Generate inputs and labels using multiple linspaces and a given mapping.
+
+        Args:
+        - circuit: The circuit for which the data is being generated.
+        - linspaces: A list of linspaces. Each linspace corresponds to an input.
+        - mapping: A function that maps values from linspaces to desired argument values.
+
+        Returns:
+        - inputs: Torch tensor of input values.
+        - labels: Torch tensor of labels generated from the circuit.
+        """
+
+        data = []
+        inputs_list = []
+
+        # Using itertools.product to generate all combinations of values from linspaces
+        for values in itertools.product(*linspaces):
+            data.append(
+                self.circuits[circuit].get_quantum_output(argvals=mapping(*values))
+            )
+            inputs_list.append(values)
+
+        inputs = torch.tensor(inputs_list, dtype=torch.float32)
+        labels = torch.tensor(data, dtype=torch.float32)
+
+        return inputs, labels
+
+
 def main():
     # init QcDSL and QcNet
     C = {"in": 2, "out": 4}
 
-    # a convenient way of initializing quantum circuits
-    dsl = """
+    T = QcNetTrainer(C)
+    T.add_dsl(
+        """
     ry, theta1, 0
     ry, theta2, 1
     cx, 0, 1
     measure_all
     """
-
-    another_dsl = """
+    )
+    T.add_dsl(
+        """
     h, 0
     ry, theta1, 1
     cx, 0, 1
@@ -83,45 +160,21 @@ def main():
     cx, 1, 0
     measure_all
     """
-
-    Q = QcDSL(
-        another_dsl,
-        C,
     )
-    N = QcNet(C)
 
     # inputs and labels
-    thetas = np.linspace(0, 2 * np.pi, 50)
-    data = [
-        Q.get_quantum_output(argvals={"theta1": theta1, "theta2": theta2})
-        for theta1 in thetas
-        for theta2 in thetas
-    ]
-    inputs = torch.tensor(
-        [[theta1, theta2] for theta1 in thetas for theta2 in thetas],
-        dtype=torch.float32,
+    selC = 0
+    inputs, labels = T.gen_data_from_linspace(
+        selC,
+        [np.linspace(0, 2 * np.pi, 50), np.linspace(0, 2 * np.pi, 50)],
+        mapping=lambda theta1, theta2: {"theta1": theta1, "theta2": theta2},
     )
-    labels = torch.tensor(data, dtype=torch.float32)
-
-    optimizer = optim.Adam(N.parameters(), lr=0.01)
-    criterion = nn.MSELoss()
 
     # train
-    for epoch in range(5000):
-        optimizer.zero_grad()
-        outputs = N(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+    T.train(inputs, labels)
 
     # inference
-    with torch.no_grad():
-        test_theta1, test_theta2 = np.pi / 4, np.pi / 3
-        prediction = N(torch.tensor([[test_theta1, test_theta2]]))
-        print(
-            f"For theta1 = {test_theta1} and theta2 = {test_theta2}, predicted probabilities: {prediction.numpy()[0]}\n"
-            f"Actual probabilities: {Q.get_quantum_output({'theta1': test_theta1, 'theta2': test_theta2})}"
-        )
+    T.infer(selC, theta1=np.pi / 4, theta2=np.pi / 3)
 
 
 if __name__ == "__main__":
